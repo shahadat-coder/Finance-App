5f5cd309d07cb43b0c6305f8333123fd28b7847c
Md.Shahadat Hossain <112892447+shahadat349850@users.noreply.github.com>
2023-12-25 21:44:10 +0600

    statistics has added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/home.dart';
+import 'screens/statistics.dart';
 
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  HomePage(),
+      home:  Statistics(),
     );
   }
 }
